087e11e555412a8d8479ed49947352e226473100
--get-all <valentin.chion@ext.theodo.com>
2025-02-20 10:34:35 +0100

    Add react app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,16 @@
 import "./intro.css";
 
-export default function Home() {
+function App() {
     return (
         <div className="intro">
             <div>Bienvenue sur ton futur pokédex !</div>
             <div>
-                Tu vas pouvoir apprendre tout ce qu'il faut sur Next.js, et
+                Tu vas pouvoir apprendre tout ce qu'il faut sur React, et
                 attraper des pokemons !
             </div>
             <div>Commence par créer ton premier pokemon: Mew !</div>
         </div>
     );
 }
+
+export default App;
