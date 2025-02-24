8ce465840848b4631ff07ea2c8a8163cf6723349
--get-all <valentin.chion@ext.theodo.com>
2025-02-24 10:45:47 +0100

    Add Loader component for training

---
+++ react-app/src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import LoaderLogo from "./loader-logo.gif";
+
+export default function Loader() {
+    return (
+        <div>
+            <img src={LoaderLogo} alt="loader" />
+        </div>
+    );
+}
